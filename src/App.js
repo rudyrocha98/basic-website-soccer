c37eec71cc5c81b04741a30623467c9c744ec7a2
Rudy Rocha <rudyaldair@hotmail.com>
2017-10-20 19:18:30 -0500

    Routing, 404 state, Components-per-View, Transition Added.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
-
-import Styles from './styles.css'
+import firebase from 'firebase';
 
 import Routes from './Routes'
+
+var config = {
+apiKey: "AIzaSyAPq7Y_RLPUYCth2pV3uDHnw2Ldwr92G9I",
+authDomain: "cfm-website.firebaseapp.com",
+databaseURL: "https://cfm-website.firebaseio.com",
+projectId: "cfm-website",
+storageBucket: "",
+messagingSenderId: "713196352726"
+};
+
+firebase.initializeApp(config);
 
 class App extends Component {
 	render() {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+
+import styles from './styles.css'
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
